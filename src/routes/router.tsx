ea4c85780a561b39dc1a28080987fb9af2de360c
Денис Касперович <kasperovich.deonis@gmail.com>
2024-09-11 22:34:51 +0300

    refactor code and write a route for the error, ProtectedPage, login

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -4,7 +4,7 @@ import {
     createBrowserRouter,
     RouterProvider,
     Route,
-    Link,
+    Link, Navigate, Outlet,
 } from "react-router-dom";
 import App from "../App";
 import {Error404} from "../components/pages/Error404";
@@ -13,6 +13,8 @@ import {Puma} from "../components/pages/Puma";
 import {Abibas} from "../components/pages/Abibas";
 import {Prices} from "../components/pages/Prices";
 import Model from "../components/pages/Model";
+import {ProtectedPage} from "../components/pages/ProtectedPage";
+import {Login} from "../components/pages/Login";
 
 export const PATH = {
     ADIDAS: "/adidas",
@@ -20,35 +22,164 @@ export const PATH = {
     ABIBAS: "/abibas",
     PRICES: "/prices",
     MODEL: "/:name/:id",
-    ERROR: "/*"
+    PROTECTED: "/protectedPage",
+    LOGIN: "/login",
+    ERROR: "/error"
 } as const
+
+
+
+const publicRouts = [
+    {
+        index: true,
+        element: <Adidas/>,
+    },
+    {
+        path: PATH.ADIDAS,
+        element: <Adidas/>,
+    },
+    {
+        path: PATH.PUMA,
+        element: <Puma/>
+    },
+    {
+        path: PATH.ABIBAS,
+        element: <Abibas/>
+    },
+    {
+        path: PATH.PRICES,
+        element: <Prices/>
+    },
+    {
+        path: PATH.MODEL,
+        element: <Model/>,
+    },
+    {
+        path: PATH.ERROR,
+        element: <Error404/>,
+    },
+    {
+        path: PATH.LOGIN,
+        element: <Login/>,
+    }
+]
+
+const privateRouts = [
+    {
+        path: PATH.PROTECTED,
+        element: <ProtectedPage/>
+    },
+]
+
+const ProtectedRoute = () => {
+    const isAuth = false
+    return isAuth ? <Outlet/> : <Navigate to={PATH.LOGIN}/>
+};
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App/>,
-        errorElement: <Error404/>,
+        errorElement: <Navigate to={PATH.ERROR}/>,
         children: [
             {
-                path: PATH.ADIDAS,
-                element: <Adidas/>,
+
+                element: <ProtectedRoute/>,
+                children: privateRouts
             },
-            {
-                path: PATH.PUMA,
-                element: <Puma/>
-            },
-            {
-                path: PATH.ABIBAS,
-                element: <Abibas/>
-            },
-            {
-                path: PATH.PRICES,
-                element: <Prices/>
-            },
-            {
-                path: PATH.MODEL,
-                element: <Model/>,
-            }
+            ...publicRouts
+
         ]
     }
 ]);
+
+
+
+
+
+
+
+//----------------------------------------------------------------------------
+
+
+
+
+//
+// import * as React from "react";
+// import {createRoot} from "react-dom/client";
+// import {
+//     createBrowserRouter,
+//     RouterProvider,
+//     Route,
+//     Link, Navigate,
+// } from "react-router-dom";
+// import App from "../App";
+// import {Error404} from "../components/pages/Error404";
+// import {Adidas} from "../components/pages/Adidas";
+// import {Puma} from "../components/pages/Puma";
+// import {Abibas} from "../components/pages/Abibas";
+// import {Prices} from "../components/pages/Prices";
+// import Model from "../components/pages/Model";
+// import {ProtectedPage} from "../components/pages/ProtectedPage";
+// import ProtectedRoute from "./ProtectedRoute";
+// import {Login} from "../components/pages/Login";
+//
+// export const PATH = {
+//     ADIDAS: "/adidas",
+//     PUMA: "/puma",
+//     ABIBAS: "/abibas",
+//     PRICES: "/prices",
+//     MODEL: "/:name/:id",
+//     PROTECTED: "/protectedPage",
+//     LOGIN: "/login",
+//     ERROR: "/error"
+// } as const
+//
+//
+//
+// export const router = createBrowserRouter([
+//     {
+//         path: "/",
+//         element: <App/>,
+//         errorElement: <Navigate to={PATH.ERROR}/>,
+//         children: [
+//             {
+//                 index: true,
+//                 element: <Adidas/>,
+//             },
+//             {
+//                 path: PATH.ADIDAS,
+//                 element: <Adidas/>,
+//             },
+//             {
+//                 path: PATH.PUMA,
+//                 element: <Puma/>
+//             },
+//             {
+//                 path: PATH.ABIBAS,
+//                 element: <Abibas/>
+//             },
+//             {
+//                 path: PATH.PRICES,
+//                 element: <Prices/>
+//             },
+//             {
+//                 path: PATH.MODEL,
+//                 element: <Model/>,
+//             },
+//             {
+//                 path: PATH.PROTECTED,
+//                 element: <ProtectedRoute><ProtectedPage/></ProtectedRoute>,
+//             },
+//             {
+//                 path: PATH.ERROR,
+//                 element: <Error404/>,
+//             },
+//             {
+//                 path: PATH.LOGIN,
+//                 element: <Login/>,
+//             }
+//
+//         ]
+//     }
+// ]);
